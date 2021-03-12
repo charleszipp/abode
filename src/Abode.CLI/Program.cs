b77533fd913d221ef09e444a8b4c979d25eeef3b
Charles Zipp <chzipp@microsoft.com>
2021-03-12 09:53:03 -0600

    setup connection with config on cli

--- src/Abode.CLI/Program.cs
+++ src/Abode.CLI/Program.cs
@@ -1,23 +1,42 @@
 ﻿using Azure.DigitalTwins.Core;
 using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Abode.CLI
 {
     class Program
     {
-        private const string _adtHostName = "adt-leo-dev-usea-czp.api.eus.digitaltwins.azure.net";
-
-        static void Main(string[] args)
+        static async Task Main()
         {
-            var creds = new DefaultAzureCredential();
-            var endpoint = new Uri($"https://{_adtHostName}");
-            var client = new DigitalTwinsClient(endpoint, creds);
-        }
-    }
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+            Uri adtInstanceUrl;
+            try
+            {
+                // Read configuration data from the 
+                IConfiguration config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                    .Build();
+                adtInstanceUrl = new Uri(config["instanceUrl"]);
+            }
+            catch (Exception ex) when (ex is FileNotFoundException || ex is UriFormatException)
+            {
+                Log.Error($"Could not read configuration. Have you configured your ADT instance URL in appsettings.json?\n\nException message: {ex.Message}");
+                return;
+            }
 
-    public class House
-    {
-        public int SquareFeet { get; set; }
+            Log.Information("Authenticating...");
+
+            var creds = new DefaultAzureCredential();
+            var client = new DigitalTwinsClient(adtInstanceUrl, creds);
+
+            Log.Information("Service client created - ready!");
+        }
     }
 }
