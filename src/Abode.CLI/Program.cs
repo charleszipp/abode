1b7eb9eadb3b97f0c65f2df27ba351b88857c494
Charles Zipp <chzipp@microsoft.com>
2021-03-12 09:25:50 -0600

    added basic models

--- src/Abode.CLI/Program.cs
+++ src/Abode.CLI/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using System;
 
 namespace Abode.CLI
 {
     class Program
     {
+        private const string _adtHostName = "adt-leo-dev-usea-czp.api.eus.digitaltwins.azure.net";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var creds = new DefaultAzureCredential();
+            var endpoint = new Uri($"https://{_adtHostName}");
+            var client = new DigitalTwinsClient(endpoint, creds);
         }
+    }
+
+    public class House
+    {
+        public int SquareFeet { get; set; }
     }
 }
---
+++ src/Abode.Domain/Floor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Abode.Domain
+{
+    public class Floor
+    {
+        // public List<Room> Rooms { get; set; }
+    }
+}
